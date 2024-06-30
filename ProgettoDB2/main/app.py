12e7f6dd777bd4474eede57b8b113f9c2e295d4e
luicons01 <luicons01@gmail.com>
2024-06-30 10:22:15 +0200

    Aggiunta dataset

--- ProgettoDB2/main/app.py
+++ ProgettoDB2/main/app.py
@@ -1,2 +1,16 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from pymongo import MongoClient
+
+app = Flask(__name__)
+
+# Configura MongoDB
+client = MongoClient("mongodb://localhost:27017/")
+db = client["SafeSleep"]
+collection = db["sleep_data"]
+
+@app.route('/')
+def home():
+    return "Welcome to the Sleep Data API!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
