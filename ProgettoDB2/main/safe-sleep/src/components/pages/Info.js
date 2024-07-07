c8d5fb1352c5e90a4cdba4ff2042b920c2cb614e
luicons01 <luicons01@gmail.com>
2024-07-07 19:16:02 +0200

    Merge branch 'Luigi'

---
+++ ProgettoDB2/main/safe-sleep/src/components/pages/Info.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+import MainLayout from '../layout/MainLayout';
+
+const Title = styled.h1`
+  font-size: 48px;
+  margin-bottom: 20px;
+`;
+
+const BodyText = styled.div`
+  max-width: 600px;
+  text-align: left;
+`;
+
+const Info = () => {
+  return (
+    <MainLayout>
+          <div>
+            <Title>Info</Title>
+            <BodyText>
+              Body text for your whole article or post. We'll put in some lorem ipsum to show how a filled-out page might look:
+              <br /><br />
+              Excepteur efficient emerging, minim veniam anim aute carefully curated Ginza conversation exquisite perfect nostrud nisi intricate Content. Qui international first-class nulla ut. Punctual adipisicing, essential lovely queen tempor eiusmod iure. Exclusive izakaya charming Scandinavian impeccable aute quality of life soft power pariatur Melbourne occaecat discerning. Qui wardrobe aliquid, et Porter destination Toto remarkable officia Helsinki excepteur Basset hound. Zürich sleepy perfect consectetur.
+            </BodyText>
+          </div>
+    </MainLayout>
+  );
+}
+
+export default Info;
